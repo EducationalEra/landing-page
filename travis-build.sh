0ddbe954a35725d610bc0aebb2c10ba413f041a5
Rui Vilao <rui@gymondo.de>
2018-11-10 16:09:17 +0100

    Fixes the build.

--- travis-build.sh
+++ travis-build.sh
@@ -10,27 +10,27 @@ fi
 
 # Generate the redirects.
 while read i; do
-    from=`echo $i | awk '{print $1}'`
-    to=`echo $i | awk '{print $2}'`
-    mkdir -p build/$from
-    cat << EOT > build$from/index.html
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <meta http-equiv="Refresh" content="0;url=$to">
-            </head>
-        </html>
-    EOT
+from=`echo $i | awk '{print $1}'`
+to=`echo $i | awk '{print $2}'`
+mkdir -p build/$from
+cat << EOT > build$from/index.html
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Refresh" content="0;url=$to">
+</head>
+</html>
+EOT
 done < redirects.txt
 
 # Create an error.html page.
 cat << EOT > build/error.html
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <meta http-equiv="Refresh" content="0;url=https://$env/">
-        </head>
-    </html>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Refresh" content="0;url=https://$env/">
+</head>
+</html>
 EOT
 
 # Copy non index.html files to their designated folder.
@@ -44,4 +44,3 @@ find build -type f -name "*.json" -exec gzip -n -9 {} \;
 
 # Remove the extension.
 find build -name '*.gz' -type f | while read NAME ; do mv "${NAME}" "${NAME%.gz}" ; done
-

61644698fe09e7875e850c45b246628a29149808
Miracle-Aligner <soldieroftheuniverse@gmail.com>
2019-04-18 15:24:56 +0300

    added zno.jade page

--- gulpfile.js
+++ gulpfile.js
@@ -68,6 +68,9 @@ gulp.task('copy', ['sass', 'templates'], function () {
   gulp.src("public/mon.html")
       .pipe(rename("mon.html"))
       .pipe(gulp.dest("build"));
+  gulp.src("public/zno.html")
+      .pipe(rename("index.html"))
+      .pipe(gulp.dest("build/zno"));
   //gulp.src("public/mun.html")
   //    .pipe(rename("mun/index.html"))
   //    .pipe(gulp.dest("build"));
